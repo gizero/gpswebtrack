4b3d10e53ce066bbe3f2d210eb4a2097ff4a2643
Andrea Galbusera <gizero@gmail.com>
2013-05-21 20:19:02 +0200

    cleanup "dancing marker"
    * generate internally the two positions instead of passing them from
    application. multiply lat and lng by a "dancing factor"

--- map.py
+++ map.py
@@ -6,14 +6,12 @@ from gevent import monkey
 app = Flask(__name__)
 monkey.patch_all()
 
-lat1 = 45.864373
-lng1 = 9.430089
-lat2 = 45.864373
-lng2 = 9.431195
+lat = 45.864373
+lng = 9.430089
 
 @app.route("/")
-def map(lat1=lat1, lng1=lng1, lat2=lat2, lng2=lng2):
-  return render_template('map.html', lat1=lat1, lng1=lng1, lat2=lat2, lng2=lng2)
+def map(lat=lat, lng=lng):
+  return render_template('map.html', lat=lat, lng=lng)
 
 @werkzeug.serving.run_with_reloader
 def run_dev_server():
