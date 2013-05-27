33ba7f4a2c5468faa14077e84a1e61e407d6f4cc
Andrea Galbusera <gizero@gmail.com>
2013-05-27 19:37:19 +0200

    gpsclient.py: make it a module
    * wrap main loop into a function
    * add an optional callback argument. If provided, it gets called
    when new data is available
    * run main loop when the module is called as a script

--- gpsclient.py
+++ gpsclient.py
@@ -1,18 +1,24 @@
 import gps
 
-debug=False
+def run(debug=False, callback=None):
 
-session = gps.gps(host='gizdesk.local')
-session.stream(gps.WATCH_ENABLE|gps.WATCH_NEWSTYLE)
+  #session = gps.gps(host='gizdesk.local')
+  session = gps.gps(host='192.168.0.10')
+  session.stream(gps.WATCH_ENABLE|gps.WATCH_NEWSTYLE)
 
-try:
-  while True:
-    report = session.next()
-    if debug:
-      print report
-    if report['class'] == 'TPV':
-      lat = report.lat
-      lon = report.lon
-      print 'LAT: %s; LON: %s' % (lat, lon)
-except StopIteration:
-  print "GPSD has terminated"
+  try:
+    while True:
+      report = session.next()
+      if debug:
+        print report
+      if report['class'] == 'TPV':
+        lat = report.lat
+        lon = report.lon
+        print 'LAT: %s; LON: %s' % (lat, lon)
+        if callback:
+          callback(lat, lon)
+  except StopIteration:
+    print "GPSD has terminated"
+
+if __name__ == "__main__":
+  run()
