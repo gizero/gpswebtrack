9e1c6ab92ad429c1cd9161d5c166b0df8809d568
Andrea Galbusera <gizero@gmail.com>
2014-05-25 19:26:44 +0200

    install script: add --system-site-packages
    * allow virtualenv to access system-wide installation of python-gps.
    This is required unless we find how to install python-gps with pip or
    similar inside virtualenv

--- venv_init.sh
+++ venv_init.sh
@@ -1,6 +1,6 @@
 # source me, don't execute me
 
 virtualenv --version || { echo "You need virtualenv!"; return 1; }
-virtualenv --distribute venv
+virtualenv --system-site-packages --distribute venv
 . venv/bin/activate
 pip install -r requirements.txt
