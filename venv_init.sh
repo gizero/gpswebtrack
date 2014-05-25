59081465e05cc549f3aaafadd9608a4f1c418989
Andrea Galbusera <gizero@gmail.com>
2014-05-25 19:26:12 +0200

    install script: fix test for virtualenv

--- venv_init.sh
+++ venv_init.sh
@@ -1,6 +1,6 @@
 # source me, don't execute me
 
-virtualenv --version >/dev/null 2>&1 || ( echo "You need virtualenv!" && return 1 )
+virtualenv --version || { echo "You need virtualenv!"; return 1; }
 virtualenv --distribute venv
 . venv/bin/activate
 pip install -r requirements.txt
